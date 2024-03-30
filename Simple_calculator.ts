cabce601db63fca8355d792e411ce52573ef9a89
Rana saqib <160149771+Saqibs20@users.noreply.github.com>
2024-03-31 01:43:16 +0500

    Add files via upload

---
+++ Simple_calculator.ts
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+
+const asnwer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  { message: "Enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operators to perform operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multipication", "Division"],
+  },
+]);
+
+// Conditional Statment
+if (asnwer.operator === "Addition") {
+    console.log(asnwer.firstNumber + asnwer.secondNumber);
+}else if (asnwer.operator === "Subtraction") {
+    console.log(asnwer.firstNumber - asnwer.secondNumber);
+}else if (asnwer.operator === "Multipication") {
+    console.log(asnwer.firstNumber * asnwer.secondNumber);
+}else if (asnwer.operator === "Division") {
+    console.log(asnwer.firstNumber / asnwer.secondNumber)
+} else {
+    console.log("Please Select Valid operator")
+}
